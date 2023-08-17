aa38f23b160992809624a1c8e597878deaa0952f
Joya Abou Jaoude <joyaaboujaoude627@gmail.com>
2023-08-17 10:16:40 +0300

    Challenge 1 - Done with Step 3

--- JavaScript Challenges/Challenge 1/script.js
+++ JavaScript Challenges/Challenge 1/script.js
@@ -1,5 +1,11 @@
 console.log('I am linked now!');
 
 var name = ('Joya');
-alert('Joya');
+alert('Joya')
 
+var age = (26);
+let lastName = ('Abou Jaoude');
+const DOB = ('september 4th');
+console.log (age);
+console.log (lastName);
+console.log (DOB);
