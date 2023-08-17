fc2c39cf005c562d3cb516a8ecc992855e12df24
Joya Abou Jaoude <joyaaboujaoude627@gmail.com>
2023-08-17 14:57:30 +0300

    Challenge 1 -  Done with Step 5

--- JavaScript Challenges/Challenge 1/script.js
+++ JavaScript Challenges/Challenge 1/script.js
@@ -1,8 +1,11 @@
+//step 1
 console.log('I am linked now!');
 
+//step 2
 var name = ('Joya');
 alert('Joya')
 
+//step 3
 var age = (26);
 let lastName = ('Abou Jaoude');
 const DOB = ('september 4th');
@@ -10,6 +13,7 @@ console.log (age);
 console.log (lastName);
 console.log (DOB);
 
+//step 4
 let String = ("This is a text");
 console.log (String);
 let BooleanT = (`true`);
@@ -27,11 +31,37 @@ const object = {
     lastName : 'Abou Jaoude',
     age : 26,
 }
-console.log (object.firstName, object.lastName, object.age)
-const a = ''
-const b = null
-console.log(b)
-console.log (a)
-console.log (array.name)
+console.log (object.firstName, object.lastName, object.age);
+const a = '';
+const b = null;
+console.log(b);
+console.log (a);
+console.log (array.name);
+
+
+//step 5
+let myText = ('challenge 1 -step 5');
+let double = ("hello");
+let single = ('helloo');
+let backticks =(`hellooo`);
+
+const counter = 3;
+  const backtick = `This is my text using backticks ${counter} times`;
+  const singleQuote =
+    'This is my text using single quotations ${counter} times';
+  const doubleQuote =
+    'This is my text using double quotations ${counter} times';
+  const tryThisAlso = `This is my new version, it’s the ${2 + 3}th version`;
+  console.log (backtick);
+  console.log (singleQuote);
+  console.log (doubleQuote);
+  console.log (tryThisAlso);
+  const explanation = ('In js single and double quotes are used to create strings however using backticks makes it easier for string operations.');
+  console.log (explanation)
+
+  //Step 6
+
+
+
 
 
