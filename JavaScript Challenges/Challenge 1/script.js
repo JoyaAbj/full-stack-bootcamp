aface6a17e3398c3f06415fb7942f74fbf56fa8d
Joya Abou Jaoude <joyaaboujaoude627@gmail.com>
2023-08-17 11:22:43 +0300

    Challenge 1 - Done with Step 4

--- JavaScript Challenges/Challenge 1/script.js
+++ JavaScript Challenges/Challenge 1/script.js
@@ -9,3 +9,29 @@ const DOB = ('september 4th');
 console.log (age);
 console.log (lastName);
 console.log (DOB);
+
+let String = ("This is a text");
+console.log (String);
+let BooleanT = (`true`);
+console.log (BooleanT);
+let BooleanF = ('false');
+console.log (BooleanF);
+let number = (1215);
+console.log (number);
+let Num = ("999");
+console.log (Num);
+const array = ['name', 'last name' , 'age'];
+console.log (array);
+const object = {
+    firstName : 'Joya',
+    lastName : 'Abou Jaoude',
+    age : 26,
+}
+console.log (object.firstName, object.lastName, object.age)
+const a = ''
+const b = null
+console.log(b)
+console.log (a)
+console.log (array.name)
+
+
