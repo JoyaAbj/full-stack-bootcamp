687318bf84225a87f0374d5cf67db315e426615d
Joya Abou Jaoude <joyaaboujaoude627@gmail.com>
2023-08-22 15:21:54 +0300

    Challenge 4 - Done with step 3

--- JavaScript Challenges/Challenge 4/challenge4.js
+++ JavaScript Challenges/Challenge 4/challenge4.js
@@ -2,10 +2,10 @@
 let fiveNumbers = [4, 9, 19, 22, 29];
 console.log (fiveNumbers);
 const personalInfo = {
-    name: "Joya",
+    fname: "Joya",
     age: 26,
     height: "152",
-    location: "Jouret el Ballout",
+    locations: "Jouret el Ballout",
     nationality: "Lebanese",
 };
 console.log("I am " + personalInfo.name);
@@ -23,6 +23,18 @@ const fullInfo = {
     ...personalInfo,
     ...additionalInfo,
 };
+fullInfo.sort
 console.log (fullInfo);
+
+//step 3
+const [firstNum,secondNum,thirdNum] = fiveNumbers;
+console.log (firstNum, secondNum,thirdNum);
+
+const {fname, nationality, locations} = personalInfo;
+console.log (fname);
+console.log (nationality);
+console.log (locations);
+
+
 
 
