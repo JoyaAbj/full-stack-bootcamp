d9b3fbcaeede9684959633a0d0690ad86de9e13f
Joya Abou Jaoude <joyaaboujaoude627@gmail.com>
2023-08-22 14:43:11 +0300

    Challenge 4 - Done with step 1

---
+++ JavaScript Challenges/Challenge 4/challenge4.js
@@ -0,0 +1,13 @@
+let fiveNumbers = [4, 9, 19, 22, 29];
+console.log (fiveNumbers);
+const personalInfo = {
+    name: "Joya",
+    age: 26,
+    height: "152",
+    location: "Jouret el Ballout",
+    nationality: "Lebanese",
+};
+console.log("I am " + personalInfo.name);
+
+
+
