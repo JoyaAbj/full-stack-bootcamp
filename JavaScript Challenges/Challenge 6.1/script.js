7c0980439d405800885dc92efe8942552020e84b
Joya Abou Jaoude <joyaaboujaoude627@gmail.com>
2023-08-29 10:16:54 +0300

    Challenge 6.1

--- JavaScript Challenges/Challenge 6.1/script.js
+++ JavaScript Challenges/Challenge 6.1/script.js
@@ -10,4 +10,48 @@ function delay(milliseconds) {
     .then(() => {
       console.log("After delay"); 
     });
-    
+ 
+    fetch("https://uselessfacts.jsph.pl/random.json?language=en")
+    .then(response => response.json())
+    .then(data => {
+    const fact = data.text;
+    console.log("Random Fact:", fact);
+  })
+  .catch(error => {
+    console.error("Error fetching random fact:", error);
+  });
+
+  async function fetchRandomFact() {
+    try {
+        const response = await fetch("https://uselessfacts.jsph.pl/random.json?language=en");
+        const data = await response.json();
+        const randomFact = data.text;
+        console.log("Random Fact:", randomFact);
+    } catch (error) {
+        console.error("An error occurred:", error);
+    }
+}
+
+fetchRandomFact();
+
+const imageUrls = [
+    "https://dog.ceo/api/breeds/image/random",
+    "https://dog.ceo/api/breeds/image/random",
+    "https://dog.ceo/api/breeds/image/random"
+];
+
+async function fetchImages(urls) {
+    try {
+        const responses = await Promise.all(urls.map(url => fetch(url)));
+        const responseData = await Promise.all(responses.map(response => response.json()));
+
+        const imageUrls = responseData.map(data => data.message);
+
+        console.log("Fetched Images:", imageUrls);
+    } catch (error) {
+        console.error("An error occurred:", error);
+    }
+}
+
+fetchImages(imageUrls);
+
